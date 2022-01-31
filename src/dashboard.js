c3efa20e25547d9e9c44960f6ca84e4ba2368dbd
Daniella Dutra <danivetdutra@gmail.com>
2022-01-31 14:58:03 -0300

    Criado grafico PieChart

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,7 +1,32 @@
+import Chart from 'react-google-charts';
 import './dashboard.css';
+import { useState } from 'react';
 
 function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const TITULO = 'Quantidade de cadastros realizados no primeiro semestre';
+
+  const [dados, setDados] = useState([
+    ['Mês', 'Quantidade'],
+    ['Janeiro', 33],
+    ['Fevereiro', 68],
+    ['Março', 49],
+    ['Abril', 15],
+    ['Maio', 80],
+    ['Junho', 27],
+  ]);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>;
+      <Chart
+        width={'400px'}
+        height={'300px'}
+        chartType={'PieChart'}
+        data={dados}
+        options={{ title: TITULO }}
+      />
+    </div>
+  );
 }
 
 export default Dashboard;
